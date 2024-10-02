b28cdddc1b1fe7d406dda451893f327061d9aaee
JazziB14 <bootht14@gmail.com>
2024-10-02 13:16:08 -0400

    project setup complete

--- counter.js
+++ counter.js
@@ -6,4 +6,6 @@ export function setupCounter(element) {
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
+
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,6 @@
        theme: {
          extend: {},
        },
-       plugins: [require('daisyui'),],
+       plugins: [require('daisyui')],
+
      }
