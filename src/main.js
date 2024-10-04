59e787ef9046579b2d85aaeefa2c81113d2d93c2
JazziB14 <bootht14@gmail.com>
2024-10-04 17:34:44 -0400

    Add TailwindCSS and JS file paths in index.html
    Changed the CSS file path from "./src/output.css" to "./dist/styles.css" and adjusted the JavaScript file path from "/main.js" to "/src/main.js". This ensures the correct files are loaded when the application is run.

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
- <div>
-  <div class="p-16 space-y-8">
-  
-    <h1 class="text-5xl">Welcome to Fit Track</h1>
-    
-    <button class="btn btn-primary w-1/2">Start Now</button>
-  </div>
-</div>
+<!-- <div>-->
+<!--  <div class="p-16 space-y-8">-->
+<!--  -->
+<!--    <h1 class="text-5xl">Welcome to Fit Track</h1>-->
+<!--    -->
+<!--    <button class="btn btn-primary w-1/2">Start Now</button>-->
+<!--  </div>-->
+<!--</div>-->
 `
 
 
