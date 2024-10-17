b4e6dc77cd828b1c8e10469fc5807713b483f75b
JazziB14 <bootht14@gmail.com>
2024-10-17 01:08:22 -0400

    Add Vite configuration file for multi-page support
    This new Vite configuration enables the build setup for multiple HTML entry points. It defines different input paths for main, progress, settings, community, and add-goal HTML pages.

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        progress: resolve(__dirname, 'progress/index.html'),
+        settings: resolve(__dirname, 'settings/index.html'),
+        community: resolve(__dirname, 'community/index.html'),
+        'add-goal': resolve(__dirname, 'add-goal/index.html'),
+      },
+    },
+  },
+})
