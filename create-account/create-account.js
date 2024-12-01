d4bf57c906fdba27a3337b79d67835f4dc4c2f24
JazziB14 <bootht14@gmail.com>
2024-11-30 21:10:34 -0500

    Add images and enhance HTML structure
    Introduced new image files for use in community posts. Improved the overall HTML structure by adding and rearranging elements for better layout and readability. Removed unnecessary elements and incorporated Tailwind and DaisyUI for style enhancements.

--- create-account/create-account.js
+++ create-account/create-account.js
@@ -1,11 +1,10 @@
 import {createAccount} from "../login/auth.js";
 
 document.querySelector('#create-account-form')?.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const email = document.querySelector('#create-email').value
-  const password = document.querySelector('#create-password').value
+    e.preventDefault()
+    const email = document.querySelector('#create-email').value
+    const password = document.querySelector('#create-password').value
 
-  // console.log(email)
-  const result = await createAccount(email, password)
-  alert(result.message || 'Account created successfully!');
+    const result = await createAccount(email, password)
+    alert(result?.message || 'Account created successfully!');
 })
