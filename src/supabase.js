f05aebff0e897f4c8899f8aac4e43c229f45ac44
tajazziahb <bootht14@gmail.com>
2025-05-29 23:27:36 -0400

    Update Supabase dependencies and credentials

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your own Supabase project credentials
-const supabaseUrl = 'https://awsdvxnbfhojmitwsihh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3c2R2eG5iZmhvam1pdHdzaWhoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODA2MjM2NSwiZXhwIjoyMDQzNjM4MzY1fQ.KNlnjXSXjJVonZVvkYwyewC1vtOmrF7KN09DCmS7Log'
+const supabaseUrl = 'https://cfvbglqoynlbnizdkuqg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmdmJnbHFveW5sYm5pemRrdXFnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODU3Mjc1OCwiZXhwIjoyMDY0MTQ4NzU4fQ.5UiD0_0DPANpXa-dihOPqNTPwgAFWbKxmH1Asdl-hl0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
