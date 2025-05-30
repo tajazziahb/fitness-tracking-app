14239974983e4830ed140cbbf3953f51a47c9d0e
tajazziahb <bootht14@gmail.com>
2025-05-30 01:36:41 -0400

    Update stylesheet links to use output.css and refine Supabase client setup

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,12 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your own Supabase project credentials
-const supabaseUrl = 'https://cfvbglqoynlbnizdkuqg.supabase.co'
+const supabaseUrl ='https://cfvbglqoynlbnizdkuqg.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmdmJnbHFveW5sYm5pemRrdXFnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODU3Mjc1OCwiZXhwIjoyMDY0MTQ4NzU4fQ.5UiD0_0DPANpXa-dihOPqNTPwgAFWbKxmH1Asdl-hl0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+supabase.auth.signUp({
+    email: 'test@example.com',
+    password: 'testpassword'
+  }).then(console.log)
